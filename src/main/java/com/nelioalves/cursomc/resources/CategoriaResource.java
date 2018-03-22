ef96f843d34a4bccbbad2fea6b177dc94e8ac7b9
Nelio Alves <acenelio@gmail.com>
2018-03-22 18:15:09 -0300

    REST testado

---
+++ src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
@@ -0,0 +1,15 @@
+package com.nelioalves.cursomc.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/categorias")
+public class CategoriaResource {
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public String listar() {
+		return "REST está funcionando!";
+	}
+}

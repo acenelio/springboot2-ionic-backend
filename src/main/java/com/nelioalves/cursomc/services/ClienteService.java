efcc0cb5923e410da6a8dfab4912c4a5dea405ab
Nelio Alves <acenelio@gmail.com>
2018-03-28 10:18:52 -0300

    Bonus: ajustando tamanho da imagem

--- src/main/java/com/nelioalves/cursomc/services/ClienteService.java
+++ src/main/java/com/nelioalves/cursomc/services/ClienteService.java
@@ -51,6 +51,9 @@ public class ClienteService {
 	@Value("${img.prefix.client.profile}")
 	private String prefix;
 	
+	@Value("${img.profile.size}")
+	private Integer size;
+	
 	public Cliente find(Integer id) {
 		
 		UserSS user = UserService.authenticated();
@@ -127,6 +130,9 @@ public class ClienteService {
 		}
 		
 		BufferedImage jpgImage = imageService.getJpgImageFromFile(multipartFile);
+		jpgImage = imageService.cropSquare(jpgImage);
+		jpgImage = imageService.resize(jpgImage, size);
+		
 		String fileName = prefix + user.getId() + ".jpg";
 		
 		return s3Service.uploadFile(imageService.getInputStream(jpgImage, "jpg"), fileName, "image");
--- src/main/java/com/nelioalves/cursomc/services/ImageService.java
+++ src/main/java/com/nelioalves/cursomc/services/ImageService.java
@@ -10,6 +10,7 @@ import java.io.InputStream;
 import javax.imageio.ImageIO;
 
 import org.apache.commons.io.FilenameUtils;
+import org.imgscalr.Scalr;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -50,5 +51,19 @@ public class ImageService {
 		} catch (IOException e) {
 			throw new FileException("Erro ao ler arquivo");
 		}
+	}
+	
+	public BufferedImage cropSquare(BufferedImage sourceImg) {
+		int min = (sourceImg.getHeight() <= sourceImg.getWidth()) ? sourceImg.getHeight() : sourceImg.getWidth();
+		return Scalr.crop(
+			sourceImg, 
+			(sourceImg.getWidth()/2) - (min/2), 
+			(sourceImg.getHeight()/2) - (min/2), 
+			min, 
+			min);		
+	}
+	
+	public BufferedImage resize(BufferedImage sourceImg, int size) {
+		return Scalr.resize(sourceImg, Scalr.Method.ULTRA_QUALITY, size);
 	}
 }

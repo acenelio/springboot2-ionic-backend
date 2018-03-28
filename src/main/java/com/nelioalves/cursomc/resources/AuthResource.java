5af5a6230ae03005d2f1aec30e00c83e6cc5d742
Nelio Alves <acenelio@gmail.com>
2018-03-28 10:19:51 -0300

    Expondo o header Authorization

--- src/main/java/com/nelioalves/cursomc/resources/AuthResource.java
+++ src/main/java/com/nelioalves/cursomc/resources/AuthResource.java
@@ -31,6 +31,7 @@ public class AuthResource {
 		UserSS user = UserService.authenticated();
 		String token = jwtUtil.generateToken(user.getUsername());
 		response.addHeader("Authorization", "Bearer " + token);
+		response.addHeader("access-control-expose-headers", "Authorization");
 		return ResponseEntity.noContent().build();
 	}
 	
--- src/main/java/com/nelioalves/cursomc/security/JWTAuthenticationFilter.java
+++ src/main/java/com/nelioalves/cursomc/security/JWTAuthenticationFilter.java
@@ -58,6 +58,7 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 		String username = ((UserSS) auth.getPrincipal()).getUsername();
         String token = jwtUtil.generateToken(username);
         res.addHeader("Authorization", "Bearer " + token);
+        res.addHeader("access-control-expose-headers", "Authorization");
 	}
 	
 	private class JWTAuthenticationFailureHandler implements AuthenticationFailureHandler {

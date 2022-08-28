7f3e51c3b58da1eb3c0e21ee204825c98eae8268
leviferreira <levi.mferreira@gmail.com>
2022-08-28 19:32:14 -0300

    Atualiza dependencies e Maven para versao mais recente

--- src/test/java/com/nelioalves/cursomc/CursomcApplicationTests.java
+++ src/test/java/com/nelioalves/cursomc/CursomcApplicationTests.java
@@ -1,16 +1,15 @@
 package com.nelioalves.cursomc;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
-public class CursomcApplicationTests {
+class CursomcApplicationTests {
 
 	@Test
-	public void contextLoads() {
+	void contextLoads() {
+		Assertions.fail();
 	}
 
 }
